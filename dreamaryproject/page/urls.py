194c853df72af796dcace14247ef99c435dacd1b
이재영 <dldustn14@gmail.com>
2020-08-19 19:41:28 +0900

    Implement create and delete function

--- dreamaryproject/page/urls.py
+++ dreamaryproject/page/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     path('',views.home, name='home'),
     path('introduce', views.introduce, name='introduce'),
     path('profile/<int:designer_id>', views.detail, name="detail"),
+    path('new/',views.new, name='new'),
+    path('create/',views.create, name='create'),
+    path('delete/<int:designer_id>/',views.delete, name='delete'),
 ]
--- dreamaryproject/page/views.py
+++ dreamaryproject/page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Designer
 
 # Create your views here.
@@ -13,3 +13,24 @@ def introduce(request):
 def detail(request,designer_id):
     designer = get_object_or_404(Designer,pk=designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method=='POST':
+        post=Designer()
+        if 'image' in request.FILES:
+            post.Image=request.FILES['image']
+        post.name=request.POST['name']
+        post.address=request.POST['address']
+        post.description=request.POST['description']
+        post.save()
+
+        return redirect('detail',post.id)
+        # render는 요청과 함게 값을 넘겨줌 , redirect는 해당 주소로 이동만 한다.
+
+def delete(request,designer_id):
+    post=get_object_or_404(Designer,pk=designer_id)
+    post.delete()
+    return redirect('home')
